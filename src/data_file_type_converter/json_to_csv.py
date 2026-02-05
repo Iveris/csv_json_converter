8ccf76302dceabdf1d90be6e92bd53b7421a63c1
Iveris <iveris@no-reply.github.com>
2026-02-04 19:58:59 -0800

    more decimal places

--- src/data_file_type_converter/json_to_csv.py
+++ src/data_file_type_converter/json_to_csv.py
@@ -17,7 +17,7 @@ class JsonToCsv(AbstractConverter):
         input_file, output_file = self._create_default_file_paths(input)
         is_valid: bool = self._validate_file(input_file, output_file)
         dataframe = pd.read_json(input_file, orient='records', precise_float=True)
-        dataframe.to_csv(output_file, float_format="%.2f", index=False)
+        dataframe.to_csv(output_file, float_format="%.5f", index=False)
         # comment out the line above and uncomment the line below to add an index to each entry
         # dataframe.to_csv(output_file, float_format="%.2f", index_label="index")
         
